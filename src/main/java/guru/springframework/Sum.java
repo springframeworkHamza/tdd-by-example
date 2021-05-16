36ddc72755d346016b9494feced8b08b2d594a8f
springframeworkHamza <kizotomandera@hotmail.com>
2021-05-16 15:07:38 +0200

    Multi-Currency Money

---
+++ src/main/java/guru/springframework/Sum.java
@@ -0,0 +1,26 @@
+package guru.springframework;
+
+public class Sum implements Expression {
+    public final Expression augend;
+    public final Expression addend;
+
+    public Sum(Expression augend, Expression addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+    public Money reduce(Bank bank, String to) {
+        int amount = augend.reduce(bank,to).amount + addend.reduce(bank, to).amount;
+        return new Money(amount, to);
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
+    }
+}
